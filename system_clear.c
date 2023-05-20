fe644431e2c97ba7ad29a1d6e9b55faec82f7e5a
Agil Mughni <agilmughni1@gmail.com>
2023-05-20 12:05:47 +0700

    add function for clear screen

---
+++ system_clear.c
@@ -0,0 +1,11 @@
+#include "header.h"
+
+void system_clear(){
+
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");  // Perintah untuk membersihkan layar pada Windows
+#else
+    system("clear");  // Perintah untuk membersihkan layar pada Unix/Linux
+#endif
+
+}
